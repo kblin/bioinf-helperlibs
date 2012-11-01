4ffb79a8a6fda1fb6131696496a72860c7b9cb03
Kai Blin <kai.blin@biotech.uni-tuebingen.de>
2012-11-01 10:15:37 +0100

    Add IO wrappers helper
    Signed-off-by: Kai Blin <kai.blin@biotech.uni-tuebingen.de>

---
+++ helperlibs/wrappers/io.py
@@ -0,0 +1,29 @@
+import tempfile
+import shutil
+import os
+from os import path
+
+class TemporaryPipe(object):
+    def __init__(self, pipename="pipe"):
+        self.pipename = pipename
+        self.tempdir = None
+
+    def __enter__(self):
+        self.tempdir = tempfile.mkdtemp()
+        pipe_path = path.join(self.tempdir, self.pipename)
+        os.mkfifo(pipe_path)
+        return pipe_path
+
+    def __exit__(self, type, value, traceback):
+        if self.tempdir is not None:
+            shutil.rmtree(self.tempdir)
+
+class TemporaryFile(object):
+    def __init__(self, suffix='', prefix='tmp', dir=None, text=False):
+        self.handle, self.name = tempfile.mkstemp(suffix, prefix, dir, text)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        os.unlink(self.name)
