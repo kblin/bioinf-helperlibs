caaa4a20a713482b95101bfad527a2be35b7118d
Kai Blin <kai.blin@biotech.uni-tuebingen.de>
2012-11-13 07:57:27 +0100

    wrappers.io: New try at TemporaryPipe windows compat
    Signed-off-by: Kai Blin <kai.blin@biotech.uni-tuebingen.de>

--- helperlibs/wrappers/io.py
+++ helperlibs/wrappers/io.py
@@ -2,21 +2,7 @@ import tempfile
 import shutil
 import os
 from os import path
-
-class TemporaryPipe(object):
-    def __init__(self, pipename="pipe"):
-        self.pipename = pipename
-        self.tempdir = None
-
-    def __enter__(self):
-        self.tempdir = tempfile.mkdtemp()
-        pipe_path = path.join(self.tempdir, self.pipename)
-        os.mkfifo(pipe_path)
-        return pipe_path
-
-    def __exit__(self, type, value, traceback):
-        if self.tempdir is not None:
-            shutil.rmtree(self.tempdir)
+import sys
 
 class TemporaryFile(object):
     def __init__(self, suffix='', prefix='tmp', dir=None, text=False):
@@ -29,3 +15,31 @@ class TemporaryFile(object):
         if hasattr(self.handle, "close"):
             self.handle.close()
         os.unlink(self.name)
+
+if sys.platform in ('linux2', 'darwin'):
+    class TemporaryPipe(object):
+        def __init__(self, pipename="pipe"):
+            self.pipename = pipename
+            self.tempdir = None
+
+        def __enter__(self):
+            self.tempdir = tempfile.mkdtemp()
+            pipe_path = path.join(self.tempdir, self.pipename)
+            os.mkfifo(pipe_path)
+            return pipe_path
+
+        def __exit__(self, type, value, traceback):
+            if self.tempdir is not None:
+                shutil.rmtree(self.tempdir)
+else:
+    class TemporaryPipe(object):
+        def __init__(self, pipename=''):
+            self.handle, self.name = tempfile.mkstemp()
+
+        def __enter__(self):
+            return self.name
+
+        def __exit__(self, type, value, traceback):
+            if hasattr(self.handle, "close"):
+                self.handle.close()
+            os.unlink(self.name)
