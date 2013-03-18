dae985757be5ec6783b2b3f0245738656bee2f24
Kai Blin <kai.blin@biotech.uni-tuebingen.de>
2013-03-18 05:54:40 +0100

    bump version
    Signed-off-by: Kai Blin <kai.blin@biotech.uni-tuebingen.de>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "helperlibs",
-    version = "0.1.1",
+    version = "0.1.2",
     author = "Kai Blin",
     author_email = "kai.blin@biotech.uni-tuebingen.de",
     description = ("A collection of bioinformatics-related helper functions"),
