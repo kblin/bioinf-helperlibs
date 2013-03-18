c1487325566a9eafa1d5b00dd2654454e029fb0c
Kai Blin <kai.blin@biotech.uni-tuebingen.de>
2013-03-18 05:52:40 +0100

    Fix a docstring typo
    Signed-off-by: Kai Blin <kai.blin@biotech.uni-tuebingen.de>

--- helperlibs/tests/bio/test_seqio.py
+++ helperlibs/tests/bio/test_seqio.py
@@ -69,7 +69,7 @@ class TestSeqIO(unittest2.TestCase):
 
 
     def test__guess_seqtype_from_file_genbank_correct(self):
-        "Test gessing the sequence type from correct genbank contents"
+        "Test guessing the sequence type from correct genbank contents"
         with open(get_file_path('melanin.gbk'), 'rU') as h:
             self.assertEqual("genbank", seqio._guess_seqtype_from_file(h))
             h.seek(0)
@@ -78,7 +78,7 @@ class TestSeqIO(unittest2.TestCase):
 
 
     def test__guess_seqtype_from_file_embl_corect(self):
-        "Test gessing the sequence type from correct embl contents"
+        "Test guessing the sequence type from correct embl contents"
         with open(get_file_path('melanin.embl'), 'rU') as h:
             self.assertEqual("embl", seqio._guess_seqtype_from_file(h))
             h.seek(0)
@@ -87,7 +87,7 @@ class TestSeqIO(unittest2.TestCase):
 
 
     def test__guess_seqtype_from_file_fasta_correct(self):
-        "Test gessing the sequence type from correct fasta contents"
+        "Test guessing the sequence type from correct fasta contents"
         with open(get_file_path('melanin.fasta'), 'rU') as h:
             self.assertEqual("fasta", seqio._guess_seqtype_from_file(h))
             h.seek(0)
@@ -96,7 +96,7 @@ class TestSeqIO(unittest2.TestCase):
 
 
     def test__guess_seqtype_from_file_genbank_no_header(self):
-        "Test gessing the sequence type from a genbank file without header"
+        "Test guessing the sequence type from a genbank file without header"
         with open(get_file_path('no_header.gbk'), 'rU') as h:
             self.assertEqual("genbank", seqio._guess_seqtype_from_file(h))
             h.seek(0)
@@ -105,7 +105,7 @@ class TestSeqIO(unittest2.TestCase):
 
 
     def test__guess_seqtype_from_file_embl_no_header(self):
-        "Test gessing the sequence type from an embl file without header"
+        "Test guessing the sequence type from an embl file without header"
         with open(get_file_path('no_header.embl'), 'rU') as h:
             self.assertEqual("embl", seqio._guess_seqtype_from_file(h))
             h.seek(0)
@@ -113,7 +113,7 @@ class TestSeqIO(unittest2.TestCase):
             self.assertEqual("embl", seqio._guess_seqtype_from_file(string_seq))
 
     def test__guess_seqtype_from_file_fasta_no_header(self):
-        "Test gessing the sequence type from a fasta file without header"
+        "Test guessing the sequence type from a fasta file without header"
         with open(get_file_path('no_header.fasta'), 'rU') as h:
             self.assertEqual("fasta", seqio._guess_seqtype_from_file(h))
             h.seek(0)
@@ -121,13 +121,13 @@ class TestSeqIO(unittest2.TestCase):
             self.assertEqual("fasta", seqio._guess_seqtype_from_file(string_seq))
 
     def test__guess_seqtype_from_file_fasta_no_header_lower_case(self):
-        "Test gessing the sequence type from a lower case fasta file without header"
+        "Test guessing the sequence type from a lower case fasta file without header"
         with open(get_file_path('no_header.fasta'), 'rU') as h:
             string_seq = h.read().lower()
             self.assertEqual("fasta", seqio._guess_seqtype_from_file(string_seq))
 
     def test__guess_seqtype_from_file_raises_error(self):
-        "Test gessing the sequence type from file raises error when it fails"
+        "Test guessing the sequence type from file raises error when it fails"
         self.assertRaises(ValueError, seqio._guess_seqtype_from_file, 'bad & invalid')
 
 
